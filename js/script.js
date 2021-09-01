a7d40bcce11740bc58fd6939d259ed6c1846fc10
Sadique <mhsadique1307@gmail.com>
2021-09-02 00:33:19 +0600

    changed http to https

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const searchButton = document.getElementById('search-button');
+const container = document.getElementById('container');
 let books;
 
 
@@ -11,7 +12,7 @@ searchButton.addEventListener('click', handleClick);
 function handleClick(e) {
     const searchText = document.getElementById('search').value;
     e.preventDefault();
-    fetch(`http://openlibrary.org/search.json?q=${searchText}`)
+    fetch(`https://openlibrary.org/search.json?q=${searchText}`)
     .then(res => res.json())
     .then(data => {
         books = data.docs;
@@ -21,6 +22,16 @@ function handleClick(e) {
 
 function showBooks(books) {
     books.forEach(book => {
-        console.log(book.author_name[0]);
+        console.log(book);
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <h1>Book Name: ${book.title}</h1>
+            <h3>Author: ${book.author_name}</h3>
+            <h5>Published on: ${book.first_publish_year}</h5>
+            <h6>Published by: ${book.publisher}</h6>
+            <img src='https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg' />
+            <hr/>
+        `;
+        container.appendChild(div);
     })
 }
